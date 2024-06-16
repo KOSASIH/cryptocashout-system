b1388ffcd56802e9f1b203c64de2a15e9933ca55
KOSASIH <kosasihg88@gmail.com>
2024-06-16 10:37:07 +0700

    Delete components directory

--- components/pi_coin_wallet.py
+++
@@ -1,9 +0,0 @@
-class PiCoinWallet:
-    def __init__(self):
-        self.pi_coin_balance = 0
-
-    def add_pi_coin(self, amount):
-        self.pi_coin_balance += amount
-
-    def get_pi_coin_balance(self):
-        return self.pi_coin_balance
