dd17f96bfc23f352fcffc76e022a44863aa50a76
KOSASIH <kosasihg88@gmail.com>
2024-06-16 10:36:40 +0700

    Delete backend_services directory

--- backend_services/transaction_processing.py
+++
@@ -1,8 +0,0 @@
-class TransactionProcessing:
-    def __init__(self):
-        self.transactions = []
-
-    def process_transaction(self, transaction):
-        # Process transaction logic
-        self.transactions.append(transaction)
-        print(f"Transaction processed: {transaction}")
