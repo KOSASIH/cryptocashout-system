219ef59fc09c3925f8345db479f0ae6bfd632f2f
KOSASIH <kosasihg88@gmail.com>
2024-06-17 19:24:38 +0700

    Update environment.js

--- config/environment.js
+++ config/environment.js
@@ -5,4 +5,8 @@ module.exports = {
   DATABASE_URL: 'ongodb://localhost:27017/cryptocashout',
   BLOCKCHAIN_RPC_URL: 'https://mainnet.infura.io/v3/YOUR_PROJECT_ID',
   ANALYTICS_API_KEY: 'YOUR_ANALYTICS_API_KEY',
+  JWT_SECRET_KEY: 'YOUR_SECRET_KEY',
+  JWT_EXPIRATION_TIME: '1h',
+  PASSWORD_SALT_ROUNDS: 10,
+  TWO_FACTOR_AUTH_ENABLED: true,
 };
