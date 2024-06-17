095d0a0e50d253b7de19c337c480a1574fbd0510
KOSASIH <kosasihg88@gmail.com>
2024-06-17 19:33:17 +0700

    Update blockchain.js

--- app/services/blockchain.js
+++ app/services/blockchain.js
@@ -7,6 +7,16 @@ class BlockchainService {
     const balance = await web3.eth.getBalance(address);
     return balance;
   }
+
+  async sendTransaction(from, to, amount) {
+    const web3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io/v3/YOUR_PROJECT_ID'));
+    const transaction = await web3.eth.sendTransaction({
+      from,
+      to,
+      value: web3.utils.toWei(amount, 'ether'),
+    });
+    return transaction;
+  }
 }
 
 export default BlockchainService;
