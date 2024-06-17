6e7e74f6d816d6c69a54e8574df63cea7c43680d
KOSASIH <kosasihg88@gmail.com>
2024-06-17 19:14:58 +0700

    Create environment.js

---
+++ config/environment.js
@@ -0,0 +1,8 @@
+// config/environment.js
+module.exports = {
+  NODE_ENV: 'development',
+  PORT: 3000,
+  DATABASE_URL: 'ongodb://localhost:27017/cryptocashout',
+  BLOCKCHAIN_RPC_URL: 'https://mainnet.infura.io/v3/YOUR_PROJECT_ID',
+  ANALYTICS_API_KEY: 'YOUR_ANALYTICS_API_KEY',
+};
