62e73f236792c1996238aa12556f7ab10ff39300
KOSASIH <kosasihg88@gmail.com>
2024-06-17 19:30:59 +0700

    Update user.js

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,5 +1,7 @@
 // app/controllers/user.js
 import { UserController } from '../models/user';
+import { WalletController } from '../models/wallet';
+import { TransactionController } from '../models/transaction';
 
 class UserController {
   async getUser(req, res) {
@@ -11,6 +13,17 @@ class UserController {
     const user = new UserController(req.body);
     await user.save();
     res.json(user);
+  }
+
+  async getWallet(req, res) {
+    const wallet = await WalletController.findOne({ userId: req.params.id });
+    res.json(wallet);
+  }
+
+  async createTransaction(req, res) {
+    const transaction = new TransactionController(req.body);
+    await transaction.save();
+    res.json(transaction);
   }
 }
 
